610683552f075dcd163810a27f054a84dd27c0f5
Teodor <Teodor.Sheynov@gmail.com>
2021-11-16 23:40:38 +0200

    XML Exercise

---
+++ EF-Core/XML-Exercise/CarDealerProfile.cs
@@ -0,0 +1,20 @@
+﻿using CarDealer.Models;
+using CarDealer.Dtos.Import;
+
+using AutoMapper;
+
+namespace CarDealer
+{
+    using DTO.ImportDto;
+
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            this.CreateMap<ImportSupplierDto, Supplier>();
+            this.CreateMap<ImportPartDto, Part>();
+            this.CreateMap<ImportCustomerDto, Customer>();
+            this.CreateMap<ImportSaleDto, Sale>();
+        }
+    }
+}
---
+++ EF-Core/XML-Exercise/StartUp.cs
@@ -0,0 +1,284 @@
+﻿namespace CarDealer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Xml.Serialization;
+
+    using AutoMapper;
+
+    using Data;
+    using DTO.ExportDto;
+    using DTO.ImportDto;
+    using Dtos.Import;
+    using Models;
+
+    public class StartUp
+    {
+        private static readonly IMapper mapper = new Mapper(new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<CarDealerProfile>();
+        }));
+
+        public static void Main(string[] args)
+        {
+            CarDealerContext dbContext = new CarDealerContext();
+
+            //ResetDb(dbContext);
+
+            //string inputXml = File.ReadAllText("../../../Datasets/sales.xml");
+            string result = GetSalesWithAppliedDiscount(dbContext);
+
+            Console.WriteLine(result);
+        }
+
+        //Problem 09
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute("Suppliers");
+            XmlSerializer xmlSerializer = new XmlSerializer(
+                typeof(ImportSupplierDto[]), xmlRoot);
+
+            //Why? Bcos XmlSerializer library is old and is old styled...
+            //Allow us to read the inputXml string chunk by chunk
+            using StringReader stringReader = new StringReader(inputXml);
+
+            ImportSupplierDto[] dtos = (ImportSupplierDto[])
+                xmlSerializer.Deserialize(stringReader);
+
+            ICollection<Supplier> suppliers = new HashSet<Supplier>();
+            foreach (ImportSupplierDto supplierDto in dtos)
+            {
+                Supplier s = new Supplier()
+                {
+                    Name = supplierDto.Name,
+                    IsImporter = bool.Parse(supplierDto.IsImporter)
+                };
+
+                suppliers.Add(s);
+            }
+
+            //If this pass
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count}";
+        }
+
+        //Problem 10
+        public static string ImportParts(CarDealerContext context, string inputXml)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute("Parts");
+            XmlSerializer xmlSerializer = new XmlSerializer(
+                typeof(ImportPartDto[]), xmlRoot);
+
+            using StringReader stringReader = new StringReader(inputXml);
+
+            ImportPartDto[] partDtos = (ImportPartDto[])
+                xmlSerializer.Deserialize(stringReader);
+
+            ICollection<Part> parts = new HashSet<Part>();
+            foreach (ImportPartDto partDto in partDtos)
+            {
+                Supplier supplier = context
+                    .Suppliers
+                    .Find(partDto.SupplierId);
+
+                if (supplier == null)
+                {
+                    continue;
+                }
+
+                Part p = new Part()
+                {
+                    Name = partDto.Name,
+                    Price = decimal.Parse(partDto.Price),
+                    Quantity = partDto.Quantity,
+                    Supplier = supplier
+                };
+
+                parts.Add(p);
+            }
+
+            context.Parts.AddRange(parts);
+            context.SaveChanges();
+
+            return $"Successfully imported {parts.Count}";
+        }
+
+        //Problem 11
+        public static string ImportCars(CarDealerContext context, string inputXml)
+        {
+            XmlSerializer xmlSerializer = 
+                GenerateXmlSerializer("Cars", typeof(ImportCarDto[]));
+            using StringReader stringReader = new StringReader(inputXml);
+
+            ImportCarDto[] carDtos = (ImportCarDto[]) 
+                xmlSerializer.Deserialize(stringReader);
+
+            ICollection<Car> cars = new HashSet<Car>();
+            //ICollection<PartCar> partCars = new HashSet<PartCar>();
+
+            foreach (ImportCarDto carDto in carDtos)
+            {
+                Car c = new Car()
+                {
+                    Make = carDto.Make,
+                    Model = carDto.Model,
+                    TravelledDistance = carDto.TravelledDistance
+                };
+
+                ICollection<PartCar> currentCarParts = new HashSet<PartCar>();
+                foreach (int partId in carDto.Parts.Select(p => p.Id).Distinct())
+                {
+                    Part part = context
+                        .Parts
+                        .Find(partId);
+
+                    if (part == null)
+                    {
+                        continue;
+                    }
+
+                    PartCar partCar = new PartCar()
+                    {
+                        Car = c,
+                        Part = part
+                    };
+                    currentCarParts.Add(partCar);
+                }
+
+                c.PartCars = currentCarParts;
+                cars.Add(c);
+            }
+
+            context.Cars.AddRange(cars);
+            context.SaveChanges();
+
+            return $"Successfully imported {cars.Count}";
+        }
+
+        //Problem 12
+        public static string ImportCustomers(CarDealerContext context, string inputXml)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ImportCustomerDto[]), new XmlRootAttribute("Customers"));
+
+            var customersDto = (ImportCustomerDto[])xmlSerializer
+                .Deserialize(new StringReader(inputXml));
+
+            var customers = mapper.Map<Customer[]>(customersDto);
+
+            context.Customers.AddRange(customers);
+            context.SaveChanges();
+
+            return $"Successfully imported {customers.Length}";
+        }
+
+        //Problem 13
+        public static string ImportSales(CarDealerContext context, string inputXml)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ImportSaleDto[]), new XmlRootAttribute("Sales"));
+
+            var salesDtos = (ImportSaleDto[])xmlSerializer
+                .Deserialize(new StringReader(inputXml));
+
+            //May be good to check if CustomerId exists too
+            var sales = mapper.Map<Sale[]>(salesDtos)
+                .Where(s => context.Cars.Any(c => c.Id == s.CarId))
+                .ToList();
+
+            context.Sales.AddRange(sales);
+            context.SaveChanges();
+
+            return $"Successfully imported {sales.Count}";
+        }
+
+        //Problem 14
+        public static string GetCarsWithDistance(CarDealerContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            using StringWriter stringWriter = new StringWriter(sb);
+
+            XmlSerializer xmlSerializer =
+                GenerateXmlSerializer("cars", typeof(ExportCarsWithDistanceDto[]));
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(String.Empty, String.Empty);
+
+            ExportCarsWithDistanceDto[] carsDtos = context
+                .Cars
+                .Where(c => c.TravelledDistance > 2000000)
+                .OrderBy(c => c.Make)
+                .ThenBy(c => c.Model)
+                .Take(10)
+                .Select(c => new ExportCarsWithDistanceDto()
+                {
+                    Make = c.Make,
+                    Model = c.Model,
+                    TraveledDistance = c.TravelledDistance.ToString()
+                })
+                .ToArray();
+
+            xmlSerializer.Serialize(stringWriter, carsDtos, namespaces);
+
+            return sb.ToString().TrimEnd();
+        }
+
+        //Problem 19
+        public static string GetSalesWithAppliedDiscount(CarDealerContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            using StringWriter sw = new StringWriter(sb);
+
+            XmlSerializer xmlSerializer =
+                GenerateXmlSerializer("sales", typeof(ExportSalesWithDiscountDto[]));
+
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(String.Empty, String.Empty);
+
+            ExportSalesWithDiscountDto[] dtos = context
+                .Sales
+                .Select(s => new ExportSalesWithDiscountDto()
+                {
+                    Car = new ExportSalesCarDto()
+                    {
+                        Make = s.Car.Make,
+                        Model = s.Car.Model,
+                        TraveledDistance = s.Car.TravelledDistance.ToString()
+                    },
+                    Discount = s.Discount.ToString(CultureInfo.InvariantCulture),
+                    CustomerName = s.Customer.Name,
+                    Price = s.Car.PartCars.Sum(pc => pc.Part.Price).ToString(CultureInfo.InvariantCulture),
+                    PriceWithDiscount = (s.Car.PartCars.Sum(pc => pc.Part.Price) -
+                                         s.Car.PartCars.Sum(pc => pc.Part.Price) * s.Discount / 100).ToString(CultureInfo.InvariantCulture)
+                })
+                .ToArray();
+
+            xmlSerializer.Serialize(sw, dtos, namespaces);
+
+            return sb.ToString().TrimEnd();
+        }
+
+        private static XmlSerializer GenerateXmlSerializer(string rootName, Type dtoType)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer = new XmlSerializer(
+                dtoType, xmlRoot);
+
+            return xmlSerializer;
+        }
+
+        private static void ResetDb(CarDealerContext dbContext)
+        {
+            //First call -> Db not exist
+            //Second call -> Db exists, First Delete, Then ReCreate
+            //N call
+            dbContext.Database.EnsureDeleted();
+            dbContext.Database.EnsureCreated();
+
+            Console.WriteLine("Db reset was successful!");
+        }
+    }
+}
