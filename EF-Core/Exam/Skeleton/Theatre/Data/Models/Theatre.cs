61aeafe31fa26d28416cde171cff42c90ba33e28
Teodor <Teodor.Sheynov@gmail.com>
2022-01-07 23:11:29 +0200

    modified

--- EF-Core/Exam/Skeleton/Theatre/Data/Models/Theatre.cs
+++ EF-Core/Exam/Skeleton/Theatre/Data/Models/Theatre.cs
@@ -1,9 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Theatre.Data.Models
 {
     public class Theatre
     {
+        public Theatre()
+        {
+            this.Tickets = new HashSet<Ticket>();
+        }
         [Key]
         public int Id { get; set; }
         [Required]
@@ -15,6 +20,8 @@ namespace Theatre.Data.Models
         [Required]
         [MaxLength(30)]
         public string Director { get; set; }
+
+        public virtual ICollection<Ticket> Tickets { get; set; }
         
     }
 }
